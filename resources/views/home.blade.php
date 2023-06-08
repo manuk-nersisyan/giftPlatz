364b27c2ae5246f93bac3ee2bd8cba4cee899395
Manuk Nersisyan <manuk.nersisyan@ifixgroup.com>
2023-06-08 17:39:16 +0400

    feat: install admin lte3

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    Dashboard
+@stop
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+        </div>
+    </div>
+@stop
+
+@push('js')
+    {{-- @include('sweet-alert-messages') --}}
+@endpush
