14f0239c7bf25617bb6c24c90234aa3799e9b2e5
Manuk Nersisyan <manuk.nersisyan@ifixgroup.com>
2023-06-21 16:34:07 +0400

    fix: users role table

--- database/migrations/2023_06_10_070802_add_role_id_to_users_table.php
+++ database/migrations/2023_06_10_070802_add_role_id_to_users_table.php
@@ -14,10 +14,11 @@ class AddRoleIdToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedInteger('role_id')->nullable();
-            $table->foreign('role_id')
-                ->references('id')
-                ->on('roles');
+            $table->foreignId('role_id')
+                ->nullable()
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
         });
     }
 
