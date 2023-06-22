ef3ae81bdeb8947ec543fbbd93ec9851ec32c512
Manuk Nersisyan <manuk.nersisyan@ifixgroup.com>
2023-06-22 20:47:28 +0400

    fix: role id in users issue

--- database/migrations/2023_06_10_070802_add_role_id_to_users_table.php
+++ database/migrations/2023_06_10_070802_add_role_id_to_users_table.php
@@ -30,6 +30,7 @@ class AddRoleIdToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign('users_role_id_foreign');
             $table->dropColumn('role_id');
         });
     }
